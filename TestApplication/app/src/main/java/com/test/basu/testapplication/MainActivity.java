64f9771da085a0e949548175ceb81f6cfd2a82d4
Lokesh Basu <lokesh.basu@gmail.com>
2014-12-12 19:18:53 +0530

    test run

--- TestApplication/app/src/main/java/com/test/basu/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/test/basu/testapplication/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends ActionBarActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Just to check git setup
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
