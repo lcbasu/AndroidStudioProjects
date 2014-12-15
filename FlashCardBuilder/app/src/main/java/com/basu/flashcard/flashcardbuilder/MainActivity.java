47fce1c43b34af5d22ef7fb2365c1c935ba6988d
Lokesh Basu <lokesh.basu@gmail.com>
2014-12-15 18:38:41 +0530

    test run

--- FlashCardBuilder/app/src/main/java/com/basu/flashcard/flashcardbuilder/MainActivity.java
+++ FlashCardBuilder/app/src/main/java/com/basu/flashcard/flashcardbuilder/MainActivity.java
@@ -1,17 +1,44 @@
 package com.basu.flashcard.flashcardbuilder;
 
-import android.support.v7.app.ActionBarActivity;
+import java.net.URLEncoder;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    final String myTag = "DocsUpload";
 
 
-public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(myTag, "OnCreate()");
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                postData();
+
+            }
+        });
+        t.start();
+    }
+
+
+
+    public void postData() {
+
+        String fullUrl = "https://docs.google.com/forms/d/1vekgrFrohxVW674y-cVmD4j_2aHejW9XfcMPuaA1Ds4/formResponse";
+        HttpRequest mReq = new HttpRequest();
+        String col1 = "Hello";
+        String col2 = "World";
+
+        String data = "entry.1328795732=" + URLEncoder.encode(col1);
+        String response = mReq.sendPost(fullUrl, data);
+        Log.i(myTag, response);
     }
 
 }
